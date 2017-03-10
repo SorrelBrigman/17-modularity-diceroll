5e10fe721352ba972f7933bdd765bc29fec5299b
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-10 11:04:42 -0600

    wrote math module

---
+++ dice-roll/lib/math.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const randomInt = (low, high) =>{
+  low = Math.ceil(low);
+  high = Math.floor(high);
+  return Math.floor(Math.random() * (high-low +1)) + min;
+}
+
+
+module.export = randomInt;
